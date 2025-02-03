1cbfe07f3b35d93a2733d0f4eb71ed35977939ec
Ramit Suri <ramit.suri@current.com>
2025-02-03 09:41:00 -0500

    First line
    Second line with multiplee
    lines spanning quite long
    text
    
    Even has line break

--- scripts/pr.sh
+++ scripts/pr.sh
@@ -138,8 +138,8 @@ Push()
 CreatePr()
 {
     echo "CreatePr Start"
-    title=\"$(git log -1 --format=%s)\"
-    body=\"$(git log -1 --format=%b)\"
+    title=\'$(git log -1 --format=%s)\'
+    body=\'$(git log -1 --format=%b)\'
     command="gh pr create -t $title -b $body --base $BaseBranch --assignee @me"
     echo "$command"
     if [ "$Draft" = true ]

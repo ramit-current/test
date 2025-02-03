bc37d5868f97f316de92de1821475371d978d0be
Ramit Suri <ramit.suri@current.com>
2025-02-03 09:39:00 -0500

    First line
    Please enter the commit message for your changes. Lines starting
    with '#' will be ignored, and an empty message aborts the commit.
    On branch changes-2
    Your branch is up to date with 'origin/changes-2'.
    Changes to be committed:

--- scripts/pr.sh
+++ scripts/pr.sh
@@ -139,8 +139,7 @@ CreatePr()
 {
     echo "CreatePr Start"
     title=\"$(git log -1 --format=%s)\"
-    echo $title
-    body=$(git log -1 --format=%b)
+    body=\"$(git log -1 --format=%b)\"
     command="gh pr create -t $title -b $body --base $BaseBranch --assignee @me"
     echo "$command"
     exit 1
